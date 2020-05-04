ca188fb9c24f37c0eb05d87ebf6a132982f0cca1
slorello89 <slorello89@gmail.com>
2020-05-04 12:56:11 -0400

    basic image show

---
+++ ShowImage/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Emgu.CV;
+using System.IO;
+namespace ShowImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            var zero = CvInvoke.Imread(Path.Join("resources","zero.jpg"));
+            CvInvoke.Imshow("zero", zero);
+            CvInvoke.WaitKey(0);
+        }
+    }
+}
